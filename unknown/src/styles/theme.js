7c9b2ab60e4e916ae2126c35576d70f4ab802d43
downpour07 <choiheeyoung10@gmail.com>
2024-10-20 19:06:12 +0900

    📑 :: style setting

---
+++ unknown/src/styles/theme.js
@@ -0,0 +1,6 @@
+export const color = {
+    White: "#FFFFFF",
+    Gray: ["#F8F8F8", "#F0F0F0", "#E8E8E8", "#E0E0E0", "#D4D4D4", "#C5C5C5", "#B0B0B0"],
+    Black: "#000000",
+    Green: ["#83DC99", "#76DA74", "#63C191", "#5EAE79"],
+};
