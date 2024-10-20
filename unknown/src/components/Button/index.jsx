5441aea61290debf66d813918c59a8332dd3c555
downpour07 <choiheeyoung10@gmail.com>
2024-10-20 20:00:02 +0900

    📑 :: Button Components

---
+++ unknown/src/components/Button/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import CustomText from "../../styles/CustomText";
+import { color } from "../../styles/theme";
+import constants from "../../styles/constants";
+
+const Button = ( props ) => {
+
+    return(
+        <TouchableOpacity style={Styles.container} onPress={props.onPress}>
+            <CustomText style={Styles.font} >
+                {props.innerText}
+            </CustomText>
+        </TouchableOpacity>
+    );
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        width: constants.width/1.2,
+        height: constants.height/20,
+        backgroundColor: color.Green[3],
+        borderRadius: 10,
+        fontSize: 14,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    font: {
+        color: color.White,
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
+})
+
+export default Button;
